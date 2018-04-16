0c7074aae1072044ed8c02031673b0c584771f41
catonrug <aigars.kadikis@gmail.com>
2018-04-16 11:00:41 +0300

    not tested completely

--- install-zabbix-server.sh
+++ install-zabbix-server.sh
@@ -110,7 +110,6 @@ cat > /etc/init.d/zabbix-server << EOF
 EOF
 grep -v "^#\!\/bin\/sh$" ~/zabbix-*/misc/init.d/debian/zabbix-server >> /etc/init.d/zabbix-server
 chmod +x /etc/init.d/zabbix-server
-systemctl enable zabbix-server
 
 #start zabbix agent at reboot
 cat > /etc/init.d/zabbix-agent << EOF
@@ -126,6 +125,9 @@ cat > /etc/init.d/zabbix-agent << EOF
 EOF
 grep -v "^#\!\/bin\/sh$" ~/zabbix-*/misc/init.d/debian/zabbix-agent >> /etc/init.d/zabbix-agent
 chmod +x /etc/init.d/zabbix-agent
+
+systemctl daemon-reload
+systemctl enable zabbix-server
 systemctl enable zabbix-agent
 
 #show existing configuration
@@ -210,10 +212,39 @@ sed -i "s/^.*date.timezone =.*$/date.timezone = Europe\/Riga/g" /etc/php/7.0/fpm
 grep "date.timezone" /etc/php/7.0/fpm/php.ini
 echo
 
+#restore backup
+if [ ! -d "/home/pi/backup" ]; then
+cd /home/pi/backup
+cp -R * /
+
 if [ -f "/home/pi/dbdump.bz2" ]; then
 bzcat /home/pi/dbdump.bz2 | sudo mysql zabbix
 fi
 
+#install git keys
+#allow only owner read and write to these keys
+chmod 600 ~/.ssh/id_rsa #git private key
+chmod 600 ~/.ssh/id_rsa.pub #git public key
+chmod 644 ~/.gitconfig #email and username for git
+
+#fix permissions
+chown -R zabbix:zabbix /home/zabbix
+chown -R zabbix:zabbix /usr/local/share/zabbix
+chmod 770 /usr/local/share/zabbix/externalscripts/*
+chmod 600 /home/zabbix/.my.cnf
+
+#install certboot agentls
+curl -s https://dl.eff.org/certbot-auto > /usr/bin/certbot
+chmod 770 /usr/bin/certbot
+#integrate some certbot settings
+mkdir -p /etc/letsencrypt
+echo renew-hook = systemctl reload nginx> /etc/letsencrypt/cli.ini
+
+#remove symlink - default nginx sites
+unlink /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/5d61050b753b.sn.mynetname.net /etc/nginx/sites-enabled/5d61050b753b.sn.mynetname.net
+
+fi
+
 systemctl start {zabbix-server,zabbix-agent}
 systemctl restart {php7.0-fpm,nginx}
-
